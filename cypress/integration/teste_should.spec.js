d2044c3a8eda8a6fd0e618ba00574d5ff6457a83
BarrocasVitoria <vitoriabarrocas2018@gmail.com>
2021-07-22 20:57:20 -0300

    novo commit

---
+++ cypress/integration/teste_should.spec.js
@@ -0,0 +1,27 @@
+/// <reference types = "cypress" />
+
+context('Validar menus', () => {
+  // Context fala sobre a funcionalidade
+  it('clicando no link comprar deve direcionar para a pagina de compra', () => {
+    cy.visit('http://lojaebac.ebaconline.art.br')
+    //ALIAS para dar nome ao elemento
+    //cy.get('#primary-menu > .menu-item-629 > a').contains('Comprar')
+    //cy.get('#primary-menu > .menu-item-629 > a').click()
+
+    cy.get('#primary-menu > .menu-item-629 > a').as('comprarMenuLink')
+    //cy.get('@comprarMenuLink').contains('Comprar')
+    //cy.get('@comprarMenuLink').click()
+
+    //COMBINANDO ACOES
+    //cy.get('@comprarMenuLink').contains('Comprar').click()
+    cy.get('@comprarMenuLink')
+      .contains('Comprar')
+      .and('have.attr', 'href')
+      .and('include', 'shop') // attr se ele contem o link
+    cy.get('@comprarMenuLink').click()
+    //cy.contains('Produtos')
+
+    //SHOULD
+    cy.get('.page-title').should('contain', 'Produtos')
+  })
+})
